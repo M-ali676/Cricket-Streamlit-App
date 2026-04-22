c69ef59583b9ab8231100c9f11ba02b49fb15166
Muhammad Ali <solangimuhammadali676@gmai.com>
2026-04-22 11:44:12 +0500

    this is updated

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from scipy import stats
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 from streamlit_option_menu import option_menu
 
 st.set_page_config(layout="wide")
+
 st.title("Cricket Data Analysis")
 # Load the dataset
 df=pd.read_csv("newfile.csv")
@@ -33,22 +33,42 @@ if selected == "Home":
 elif selected == "Player Analysis":
     st.title("Player Analysis")
     player = st.selectbox("Select Player", df["Player"])
-    pdata = df[df["Player"]== player]
-    stats=["100","50","4s","6s"]
-    chart_data =(
-        pdata[stats]
-        .iloc[0]
-        .reset_index()
-    )
-    chart_data.columns = ["Stat","Value"]
+    pdata = df[df["Player"]== player].reset_index()
+
+    values=pdata[["100","50","4s","6s"]].iloc[0]
 
     fig=px.bar(
-        chart_data,
-        x="Stat",
-        y="Value",
+        x=values,
+        y=values.index
+    )
+
+    fig.update_xaxes(range=[0, values.max() + 20])
+
+
+    # stats=["100","50","4s","6s"]
+    # chart_data =(
+    #     pdata[stats]
+    #     .iloc[0]
+    #     .reset_index()
+    # )
+    # chart_data.columns = ["Stat","Value"]
+
+    # fig=px.bar(
+    #     chart_data,
+    #     x="Stat",
+    #     y="Value",
         
 
-    ) 
+    # ) 
+    col4,col5,col6,col7 = st.columns(4)
+    total_runs = pdata["Runs"].sum()
+    total_matches = pdata["Matches"]
+    hundreds = pdata["100"].sum()
+    sixties = pdata["6s"].sum()
+    col4.metric(label="Total Runs", value=total_runs)
+    col5.metric(label="Total Matches", value=total_matches)
+    col6.metric(label="Total 100s", value=hundreds)
+    col7.metric(label="Total 6s", value=sixties)
 
     st.plotly_chart(fig, use_container_width=True)
     
@@ -83,4 +103,8 @@ elif selected == "Comparison":
         color="Country",
     )
     st.plotly_chart(fig, use_container_width=True)
+
+elif selected == "Data Explorer":
+        st.title("Data Explorer")
+        st.dataframe(df)
 
